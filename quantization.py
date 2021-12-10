8cafdd5923b2ea2ad21f5779cf2e16e313c60e96
dmuiruri <denonjugush@gmail.com>
2021-12-10 22:18:22 +0000

    Refactored model creation code

--- quantization.py
+++ quantization.py
@@ -16,29 +16,50 @@ mnist = tf.keras.datasets.mnist
 train_images = train_images.astype(np.float32) / 255.0
 test_images = test_images.astype(np.float32) / 255.0
 
-# Define the model architecture
-model = tf.keras.Sequential([
-  tf.keras.layers.InputLayer(input_shape=(28, 28)),
-  tf.keras.layers.Reshape(target_shape=(28, 28, 1)),
-  tf.keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(10)
-])
+def create_keras_model():
+    """
+    Create keras model
+    """
+    # Define the model architecture
+    model = tf.keras.Sequential([
+        tf.keras.layers.InputLayer(input_shape=(28, 28)),
+        tf.keras.layers.Reshape(target_shape=(28, 28, 1)),
+        tf.keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(10)
+    ])
+    
+    # Train the digit classification model
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                      from_logits=True),
+                  metrics=['accuracy'])
+    model.fit(
+        train_images,
+        train_labels,
+        epochs=5,
+        validation_data=(test_images, test_labels)
+    )
+    return model
 
-# Train the digit classification model
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(
-                  from_logits=True),
-              metrics=['accuracy'])
-model.fit(
-  train_images,
-  train_labels,
-  epochs=5,
-  validation_data=(test_images, test_labels)
-)
+def convert_to_tflite_model():
+    """
+    Convert the Tensorflow model to a tensorflow lite model
+    """
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+    return tflite_model
 
+def create_default_optimizations():
+    """
+    Enable default optimizations, only fixed parameters are converted.
+    """
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    tflite_model_quant = converter.convert()
+    return tflite_model_quant
 
 if __name__ == '__main__':
     """Add unit tests here"""
-    pass
+    model = create_keras_model()
