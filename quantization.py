f496329077c96ad2c2a51344cd6b86aeb363c448
dmuiruri <denonjugush@gmail.com>
2021-12-10 21:54:25 +0000

    Add template to move code from notebooks

---
+++ quantization.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.DEBUG)
+
+import tensorflow as tf
+import numpy as np
+assert float(tf.__version__[:3]) >= 2.3
+
+
+if __name__ == '__main__':
+    """Add unit tests here"""
+    pass
